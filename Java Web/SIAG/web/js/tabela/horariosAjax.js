0507f2b7dace48a46dc6d748f714343042442319
marcus <marcus@MarcusUbuntu>
2016-05-24 22:45:59 -0300

    22:45 | Marcus Cartágenes

--- Java Web/SIAG/web/js/tabela/horariosAjax.js
+++ Java Web/SIAG/web/js/tabela/horariosAjax.js
@@ -1,21 +1,25 @@
 var frm = $(".formHorario");
 $("#containerTable").hide();
-frm.submit(function (){
+frm.submit(function () {
     $.ajax({
         type: frm.attr("method"),
         url: frm.attr("action"),
         data: frm.serialize(),
-        success: function(data){
-            var result=data;
-            $('#bodyTable').empty();
-            $("#containerTable").show();
+        success: function (data) {
+            var result = data;
+            if (result.toString().length == 0) {
+                alert("Nenhum resultado");
+                $("#containerTable").hide();
+            } else {
+                $('#bodyTable').empty();
+                $("#containerTable").show();
                 $('#clienteTemplate').tmpl(data)
                         .appendTo('#bodyTable'); //adiciona os dados ao template e anexa ao container
                 $('#carregando').hide();
-                //console.log(listaClientes);
-            console.log(result);
+                //console.log(listaClientes);               
+            }
         }
-        
+
     });
     return false;
 });
